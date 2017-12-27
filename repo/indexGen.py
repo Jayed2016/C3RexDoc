e120a40fc1cabf55eac941a64952a4c87509a39d
Hua <binhua.tein@gmail.com>
2017-12-27 16:58:10 +0800

    Update

--- repo/indexGen.py
+++ repo/indexGen.py
@@ -5,10 +5,16 @@ myPath = op.dirname(op.realpath(__file__))
 mdF = open(op.join(myPath, "index.md"), "w")
 serverRoot = "https://rexrainbow.github.io/C3RexDoc/repo/"
 
+l = """\
+|Index|Icon|Addon name|
+|-----|----|----------|\
+"""
+mdF.write(l)
+
 names = glob.glob(op.join(myPath, "*.c3addon"))
 for idx, p in enumerate(names):
     name = op.split(p)[1]
-    l = """{idx}. <img src="{server}{iconName}" width="50" heigh="50"> [{name}]({server}{name})  \n""".format(
+    l = """|{idx}|<img src="{server}{iconName}" width="50" heigh="50">|[{name}]({server}{name})|""".format(
         idx=idx+1, 
         name=name,         
         server=serverRoot,
